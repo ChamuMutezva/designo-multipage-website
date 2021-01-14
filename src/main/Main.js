58ecb630d2701daba56c9af8815551daa1463e53
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-01-14 06:00:47 +0200

    feat: additionalInfo component - mobile section completed

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,69 +1,10 @@
-import WebMobile from "../assets/home/mobile/image-web-design.jpg"
-import WebTablet from "../assets/home/tablet/image-web-design.jpg"
-import WebDesktopSmall from "../assets/home/desktop/image-web-design-small.jpg"
-import WebDesktopLarge from "../assets/home/desktop/image-web-design-large.jpg"
-
-import AppMobile from "../assets/home/mobile/image-app-design.jpg"
-import AppTablet from "../assets/home/tablet/image-app-design.jpg"
-import AppDesktop from "../assets/home/desktop/image-app-design.jpg"
-
-import DesignMobile from "../assets/home/mobile/image-graphic-design.jpg"
-import DesignTablet from "../assets/home/tablet/image-graphic-design.jpg"
-import DesignDesktop from "../assets/home/desktop/image-graphic-design.jpg"
-
-import IconRightArrow from "../assets/shared/desktop/icon-right-arrow.svg"
-
-import './Main.css'
-
+import MainSub1 from './MainSub1'
+import MainSub2 from './MainSub2'
 const Main = () => {
-    return (
-        <div class="main__container">
-
-            <div className="projects">
-                <div className="webdesign project">
-                    <img className="project_img" src={WebMobile} alt="web design"
-                        sizes="(min-width: 1080px) 780px,
-                            (min-width: 680px) 327px, 100%"
-                        srcSet={`${WebMobile} 327w, ${WebTablet} 689w,
-                           ${WebDesktopSmall} 541w , ${WebDesktopLarge} 541W`} />
-                    <div className="project__summary">
-                        <h2 className="project__summary__title">Web design</h2>
-                        <a href="" className="project__btn">View projects
-                        <img src={IconRightArrow} className="project__btn__img" />
-                        </a>
-                    </div>
-
-                </div>
-                <div className="appdesign project">
-                    <img className="project_img" src={AppMobile} alt="App design"
-                        sizes="(min-width: 1080px) 780px,
-                            (min-width: 680px) 580px , 100%"
-                        srcSet={`${AppMobile} 327w, ${AppTablet} 689w,
-                           ${AppDesktop} 541w`} />
-
-                    <div className="project__summary">
-                        <h2 className="project__summary__title">App design</h2>
-                        <a href="" className="project__btn">View projects
-                        <img src={IconRightArrow} className="project__btn__img" />
-                        </a>
-                    </div>
-
-                </div>
-                <div className="graphicDesign project">
-                    <img className="project_img" src={DesignMobile} alt="graphic design"
-                        sizes="(min-width: 1080px) 780px,
-                            (min-width: 680px) 580px, 100%"
-                        srcSet={`${DesignMobile} 327w, ${DesignTablet} 689w,
-                           ${DesignDesktop} 541w`} />
-                    <div className="project__summary">
-                        <h2 className="project__summary__title">Graphic design</h2>
-                        <a href="" className="project__btn">View projects
-                         <img src={IconRightArrow} className="project__btn__img" />
-                        </a>
-                    </div>
-                </div>
-            </div>
-
+    return(
+        <div>
+            <MainSub1 />
+            <MainSub2 />
         </div>
     )
 }
---
+++ src/main/MainSub2.js
@@ -0,0 +1,52 @@
+import Passionate from '../assets/home/desktop/illustration-passionate.svg'
+import Resourceful from '../assets/home/desktop/illustration-resourceful.svg'
+import Friendly from "../assets/home/desktop/illustration-friendly.svg"
+
+const MainSub2 = () => {
+    return (
+        <div className="additionalInfo">
+            <figure>
+                <img src={Passionate} />
+                <figcaption>
+                    <h3>
+                        Passionate
+                    </h3>
+                    <p>
+                        Each project starts with an in-depth brand research to ensure we
+                        only create products that serve a purpose. We merge
+                        art, design, and technology into exciting new solutions.
+                    </p>
+                </figcaption>
+            </figure>
+
+            <figure>
+                <img src={Resourceful} alt=""/>
+                <figcaption>
+                    <h3>
+                        Resourceful
+                    </h3>
+                    <p>
+                        Everything that we do has a strategic purpose.
+                        We use an agile approach in all of our projects and value customer
+                        collaboration. It guarantees superior results that fulfill our clients’ needs.
+                    </p>
+                </figcaption>
+            </figure>
+
+            <figure>
+                <img src={Friendly} alt=""/>
+                <figcaption>
+                    <h3> Friendly</h3>
+                    <p>
+                        We are a group of enthusiastic folks who know how to put people first.
+                        Our success depends on our customers, and we
+                        strive to give them the best experience a company can provide.
+                    </p>
+                </figcaption>
+            </figure>
+
+
+        </div>
+    )
+}
+export default MainSub2
