4fd24d063844163bb1475d3ae1da62e90d9d3eb0
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-01-15 05:53:50 +0200

    home page - mobile footer section completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import HeaderPrimary from './header/HeaderPrimary'
 import Main from './main/Main'
+import Footer from './footer/Footer'
 import './App.css';
 
 function App() {
   return (
     <div className="App">         
         <HeaderPrimary /> 
-        <Main/>   
+        <Main/> 
+        <Footer/>  
     </div>
   );
 }
---
+++ src/footer/Footer.js
@@ -0,0 +1,78 @@
+import Logo from '../assets/shared/desktop/logo-dark.png'
+import facebook from '../assets/shared/desktop/icon-facebook.svg'
+import instagram from '../assets/shared/desktop/icon-instagram.svg'
+import twitter from '../assets/shared/desktop/icon-twitter.svg'
+import pinterest from '../assets/shared/desktop/icon-pinterest.svg'
+import youtube from '../assets/shared/desktop/icon-youtube.svg'
+import './Footer.css'
+const Footer = () => {
+    return (
+        <div class="footer">
+            <aside>
+                <div class="aside__container">
+                    <h4 class="aside__title">Let’s talk about your project</h4>
+                    <p class="aside__summary">
+                        Ready to take it to the next level? Contact us today and find out
+                        how our expertise can help your business grow.
+                    </p>
+                    <button class="aside__btn">Get in touch</button>
+                </div>
+            </aside>
+
+            <div class="mainFooter">
+                <div class="footerLogo__container">
+                    <img src={Logo} alt="" class="footerLogo" />
+                </div>
+
+                <div className="footer__menu">
+                    <ul className="footer__menuList">
+                        <li tabIndex="0"><a href="">OUR COMPANY</a></li>
+                        <li tabIndex="0"><a href="">LOCATIONS</a></li>
+                        <li tabIndex="0"><a href="">CONTACT</a></li>
+                    </ul>
+                </div>
+
+                <address className="address1 address">
+                    <p>Designo Central Office</p>
+                    <p>3886 Wellington Street</p>
+                    <p>Toronto, Ontario M9C 3J5</p>
+                </address>
+
+                <address className="address2 address">
+                    <p> Contact Us (Central Office)</p>
+                    <p> P : +1 253-863-8967</p>
+                    <p> M : contact@designo.co</p>
+                </address>
+
+                <div className="socials">
+                    <div>
+                        <a href=""> <img src={facebook} alt="" />  </a>
+                    </div>
+
+                    <div>
+                        <a href=""> <img src={instagram} alt="" />  </a>
+                    </div>
+
+                    <div>
+                        <a href=""> <img src={twitter} alt="" />  </a>
+                    </div>
+
+                    <div>
+                        <a href=""> <img src={pinterest} alt="" />  </a>
+                    </div>
+
+                    <div>
+                        <a href=""> <img src={youtube} alt="" />  </a>
+                    </div>
+                </div>
+
+            </div>
+
+
+
+
+        </div>
+    )
+}
+
+export default Footer
