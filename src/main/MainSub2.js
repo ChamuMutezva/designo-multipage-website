f0ffb0fdcc3189091a5ae829a32853e88ee8d9b1
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-01-23 23:33:03 +0200

    created components figure

--- src/main/MainSub2.js
+++ src/main/MainSub2.js
@@ -1,49 +1,25 @@
 import Passionate from '../assets/home/desktop/illustration-passionate.svg'
 import Resourceful from '../assets/home/desktop/illustration-resourceful.svg'
 import Friendly from "../assets/home/desktop/illustration-friendly.svg"
+import FigureCard from "./Figurecard"
 
 const MainSub2 = () => {
     return (
         <div className="additionalInfo">
-            <figure>
-                <img src={Passionate} alt="passionate" className="additionalInfo__img" />
-                <figcaption>
-                    <h3>
-                        Passionate
-                    </h3>
-                    <p>
-                        Each project starts with an in-depth brand research to ensure we
-                        only create products that serve a purpose. We merge
-                        art, design, and technology into exciting new solutions.
-                    </p>
-                </figcaption>
-            </figure>
+            <FigureCard src={Passionate} alt="passionate" title="Passionate"
+                summary="Each project starts with an in-depth brand research to ensure we
+            only create products that serve a purpose. We merge
+            art, design, and technology into exciting new solutions." />
 
-            <figure>
-                <img src={Resourceful} alt="resourceful" className="additionalInfo__img"/>
-                <figcaption>
-                    <h3>
-                        Resourceful
-                    </h3>
-                    <p>
-                        Everything that we do has a strategic purpose.
-                        We use an agile approach in all of our projects and value customer
-                        collaboration. It guarantees superior results that fulfill our clients’ needs.
-                    </p>
-                </figcaption>
-            </figure>
+            <FigureCard src={Resourceful} alt="resourceful" title="Resourceful"
+                summary="Everything that we do has a strategic purpose.
+                We use an agile approach in all of our projects and value customer
+                collaboration. It guarantees superior results that fulfill our clients’ needs." />
 
-            <figure>
-                <img src={Friendly} alt="friendly" className="additionalInfo__img"/>
-                <figcaption>
-                    <h3> Friendly</h3>
-                    <p>
-                        We are a group of enthusiastic folks who know how to put people first.
-                        Our success depends on our customers, and we
-                        strive to give them the best experience a company can provide.
-                    </p>
-                </figcaption>
-            </figure>
+            <FigureCard src={Friendly} alt="friendly" title="friendly"
+                summary=" We are a group of enthusiastic folks who know how to put people first.
+                Our success depends on our customers, and we
+                strive to give them the best experience a company can provide." />
 
 
         </div>
