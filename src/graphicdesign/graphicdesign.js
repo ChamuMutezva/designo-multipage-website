93eec6dbf247c75fc472c7ec443f6b339d8e78fe
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-01-25 22:39:01 +0200

    feat: graphic design page first layout done

--- src/graphicdesign/graphicdesign.js
+++ src/graphicdesign/graphicdesign.js
@@ -1,8 +1,16 @@
 import Graphicdesignheader from './graphic_design_header/GraphicDesignHeader'
+import Graphicdesignmain from './graphic_design_main/GraphicDesignMain'
+import MainSubDesign from './graphic_design_main/MainSubDesign'
+import MainFooter from '../webdesign/web_design_footer/WebDesignFooter'
 const GraphicDesign = () => {
     return (
         <div className="graphicDesign">
            <Graphicdesignheader />
+           <div className="mainSection mainSectionWeb">
+                <Graphicdesignmain/>
+                <MainSubDesign />
+            </div>
+            <MainFooter/>
         </div>
     )
 }
