e968a6c4e0b719d4672c80d281991f7975283f62
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-01-30 12:04:18 +0200

    locations component desktop image sizing

--- src/locations/Locations.js
+++ src/locations/Locations.js
@@ -1,18 +1,10 @@
 import LocationsMain from "./Locations_main"
-<<<<<<< HEAD
-import LocationMain from "./Locations_main"
-=======
->>>>>>> c91444bc5574e6bfcde7357bf9b2f3fb8fd85fe2
 import MainFooter from '../webdesign/web_design_footer/WebDesignFooter'
 const Locations = () => {
     return (
         <>
             <LocationsMain />
-<<<<<<< HEAD
-            <MainFooter/>
-=======
             <MainFooter />
->>>>>>> c91444bc5574e6bfcde7357bf9b2f3fb8fd85fe2
         </>
     )
 }
