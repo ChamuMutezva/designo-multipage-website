d6eb840fdbf7489c78af852812152d458d7036ef
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-01-31 16:17:15 +0200

    fix: adding width to the pseudo elements

--- src/contact/contact_page_header/ContactForm.js
+++ src/contact/contact_page_header/ContactForm.js
@@ -1,19 +1,23 @@
 import "../contact.css"
 import Button from "../../sharedComponents/ButtonComponent"
 const ContactForm = () => {
+    const submitForm = (evt) => {
+        evt.preventDefault()
+        console.log(evt.target)
+    }
     return (
         <div className="form__container">
-            <form>
+            <form onSubmit={submitForm}>
                 <div className="form__inputs">
                     <label className="contactDetails">
                         {/*<span class="contactMe">Name</span> */}
-                        <input type="text" name="person" id="name" placeholder="Name" />
+                        <input type="text" name="person" id="name" placeholder="Name" required />
                     </label>
                     <label className="contactDetails">
-                        <input type="email" name="email" id="email" placeholder="Email" />
+                        <input type="email" name="email" id="email" placeholder="Email" required />
                     </label>
                     <label className="contactDetails">
-                        <input type="phone" name="phone" id="phone" placeholder="0027832678210" />
+                        <input type="phone" name="phone" id="phone" placeholder="0027832678210" required />
                     </label>
                     <label className="contactDetails">
                         <textarea name="message" id="" cols="30" rows="2" placeholder="Message"></textarea>
--- src/sharedComponents/ButtonComponent.js
+++ src/sharedComponents/ButtonComponent.js
@@ -1,7 +1,7 @@
 import "./../sharedStyles/Appwebdesign.css"
 const Button = (props) => {
     return (
-        <button className="btn__component">
+        <button className="btn__component" type="submit">
             {props.title}
         </button>
     )
