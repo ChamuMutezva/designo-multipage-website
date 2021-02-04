1a3e19b9c7216a1d0aeefe553d6a699bf6fa8276
Chamunorwa Mutezva <40114498+ChamuMutezva@users.noreply.github.com>
2021-02-04 05:32:01 +0200

    form inputs data capturing

--- src/contact/contact_page_header/ContactForm.js
+++ src/contact/contact_page_header/ContactForm.js
@@ -1,9 +1,65 @@
+import { useState } from "react"
 import "../contact.css"
 import Button from "../../sharedComponents/ButtonComponent"
 const ContactForm = () => {
+    /*  const intitialState = {
+          fullname: "",
+          emailAddress: "",
+          phone: "",
+          message: ""
+      }
+      const [formData, setFormData] = useState(intitialState)
+      const onChangeHandler = (evt) => {
+          const { name, value } = evt.target
+          console.log(name, value)
+          setFormData({ ...formData, [name]: value })
+      }
+      const submitForm = (evt) => {
+          evt.preventDefault()
+          console.log(FormData)
+  
+      }
+      const { fullname, emailAddress, phone, message } = formData
+  console.log(fullname)
+  */
+    const [newName, setNewName] = useState("")
+    const [newEmail, setNewEmail] = useState("")
+    const [newPhone, setNewPhone] = useState("")
+    const [newMsg, setNewMsg] = useState("")
+    const [newData, setNewData] = useState([])
+
+    const onChangeName = (evt) => {
+        console.log(evt.target.value)
+        setNewName(evt.target.value)
+    }
+    const onChangeEmail = (evt) => {
+        console.log(evt.target.value)
+        setNewEmail(evt.target.value)
+    }
+    const onChangePhone = (evt) => {
+        console.log(evt.target.value)
+        setNewPhone(evt.target.value)
+    }
+    const onChangeMsg = (evt) => {
+        console.log(evt.target.value)
+        setNewMsg(evt.target.value)
+    }
     const submitForm = (evt) => {
         evt.preventDefault()
-        console.log(evt.target)
+        const dataObj = {
+            name: newName,
+            email: newEmail,
+            phone: newPhone,
+            msg: newMsg,
+            id: newData.length + 1
+        }
+        setNewData(newData.concat(dataObj))
+        setNewName('')
+        setNewEmail('')
+        setNewPhone('')
+        setNewMsg('')
+        console.log(newData)
+
     }
     return (
         <div className="form__container">
@@ -11,21 +67,25 @@ const ContactForm = () => {
                 <div className="form__inputs">
                     <label className="contactDetails">
                         {/*<span class="contactMe">Name</span> */}
-                        <input type="text" name="person" id="name" placeholder="Name" required />
+                        <input type="text" name="name" id="name" value={newName}
+                            placeholder="Name" required onChange={onChangeName} />
                     </label>
                     <label className="contactDetails">
-                        <input type="email" name="email" id="email" placeholder="Email" required />
+                        <input type="email" name="email" id="emailaddress" value={newEmail}
+                            placeholder="xyz@gmail.com" required onChange={onChangeEmail} />
                     </label>
                     <label className="contactDetails">
-                        <input type="tel" name="phone" id="phone" placeholder="0027832678210" required />
+                        <input type="tel" name="phone" id="phone" value={newPhone}
+                            placeholder="0027832678210" required onChange={onChangePhone} />
                     </label>
                     <label className="contactDetails">
-                        <textarea name="message" id="" cols="30" rows="2" placeholder="Message"></textarea>
+                        <textarea name="message" id="msg" cols="30" rows="2" value={newMsg}
+                            placeholder="Message" onChange={onChangeMsg}></textarea>
                     </label>
                 </div>
 
                 <div className="btn__ctrl">
-                    <Button title="Submit"/>
+                    <Button title="Submit" />
                 </div>
 
             </form>
--- src/sharedComponents/ButtonComponent.js
+++ src/sharedComponents/ButtonComponent.js
@@ -1,7 +1,7 @@
 import "./../sharedStyles/Appwebdesign.css"
 const Button = (props) => {
     return (
-        <button className="btn__component" type="submit" aria-label={props.label} tabindex="0">
+        <button className="btn__component" type="submit" aria-label={props.label} tabIndex="0">
             {props.title}
         </button>
     )
